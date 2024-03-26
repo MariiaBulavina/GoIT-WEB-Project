124cae08270df1e68f7e62cbeb7402b1bb6316e3
Mariia Bulavina <120931054+MariiaBulavina@users.noreply.github.com>
2024-03-26 01:13:00 -0500

    added qrcode creation

---
+++ src/services/qrcode_creation.py
@@ -0,0 +1,20 @@
+import qrcode
+import io
+
+
+def generate_qrcode(url):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+
+    buffer = io.BytesIO()
+    img.save(buffer, 'PNG')
+    buffer.seek(0)
+
+    return buffer
