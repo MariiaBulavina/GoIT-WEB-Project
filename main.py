576193f7dfa2cc71f774258671ac124a0a23e387
Mariia Bulavina <120931054+MariiaBulavina@users.noreply.github.com>
2024-04-05 04:13:50 -0500

    added some changes

--- main.py
+++ main.py
@@ -30,5 +30,3 @@ def read_root():
     return {'message': 'FastAPI'}
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), log_level="info")
